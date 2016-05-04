81cbf4faa08b1a95be25a1f359669cfe1ebc3eeb
noprom <tyee.noprom@qq.com>
2016-05-04 23:39:11 +0800

    init project

---
+++ src/main/java/com/huntdreams/app/App.java
@@ -0,0 +1,14 @@
+package com.huntdreams.app;
+
+/**
+ * App
+ *
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 5/4/16 11:35 PM.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Assembly in main init done.");
+    }
+}
